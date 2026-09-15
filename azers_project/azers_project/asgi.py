507e9a9eda66a64660c31c9667c0b41ae94fe533
oladipupo samuel <samueltoluwani169@gmail.com>
2026-09-15 14:12:27 +0100

    Fix ASGI startup for chat consumers

--- azers_project/azers_project/asgi.py
+++ azers_project/azers_project/asgi.py
@@ -8,17 +8,25 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
+
+# Django settings must be configured before importing routing, whose consumers
+# import Django models at module load time.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "azers_project.settings")
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import core.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "azers_project.settings")
+# Finish Django's app initialization before routing imports WebSocket consumers
+# (which import models).
+django_asgi_app = get_asgi_application()
+
+import core.routing
 
 # application = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             core.routing.websocket_urlpatterns
