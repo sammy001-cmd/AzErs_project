5eb8c57d4fb5346539ca1c466a8a97143e9709aa
oladipupo samuel <samueltoluwani169@gmail.com>
2026-09-21 14:23:19 +0100

    another error fixed

--- azers_project/azers_project/settings.py
+++ azers_project/azers_project/settings.py
@@ -155,11 +155,11 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 WHITENOISE_MANIFEST_STRICT = False
 
